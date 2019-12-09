6ec42db44cb5cdbf96d1d2516dde4ca491671199
Ed Hansen <ed.hansen@live.se>
2019-12-09 14:15:16 +0100

    final minor ajustment added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'NFL Crime';
-  name = 'Ed';
+  title = 'Tour of Heroes';
 }
